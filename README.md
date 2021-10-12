ad3363457cd547cd53a0aa5ad924f83106c7a1c7
ladyusa <usa.sa@ku.th>
2021-10-12 16:02:15 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Wisdom Book API
+
+Spring Boot example with Rest API and JPA.
+
+* Config with PostgreSQL so that it can be deployed on Free Heroku account
+* Config with H2 (commented)
---
+++ src/main/java/th/ac/ku/book/controller/BookRestController.java
@@ -0,0 +1,27 @@
+package th.ac.ku.book.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import th.ac.ku.book.model.Book;
+import th.ac.ku.book.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+public class BookRestController {
+
+    @Autowired
+    private BookService service;
+
+    @GetMapping
+    public List<Book> getBooks() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return service.create(book);
+    }
+
+}
